c9607500d1a635982e24dff3d00833b11df0b5b6
davitkv8 <davit.kv8@gmail.com>
2021-10-02 04:43:13 +0400

    djangoAPP

--- static/users/main.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById("complete-btn").addEventListener("click", verificationAlert);
-
-
-function verificationAlert(email) {
-    alert("Verification Link Has been sent to " + email)
-  }
